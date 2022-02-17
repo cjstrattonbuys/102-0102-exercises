e39d362225561223aab7b99e12edd8f7bdcc5e44
CJ Buys <cjstrattonbuys@gmail.com>
2022-02-17 09:51:14 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write your JavaScript below this line
+var numberOfHorses = "364";
+console.log("The number of horses is " + numberOfHorses);
+
+var favoriteFlower = "iris";
+console.log("My favorite flower is the " + favoriteFlower);
+
+var birthCity = "Augusta, ME";
+var birthday = "19 November 1990";
+console.log("I was born in " + birthCity + " on " + birthday);
